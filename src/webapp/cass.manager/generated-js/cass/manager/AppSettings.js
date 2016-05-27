2dc97f76c2fa0acc604565361ffee8e9c2ae1118
Ronald Ray <fritz.ray@eduworks.com>
2016-05-27 10:04:05 -0700

    Moved to Maven structure. Demo updates. Restructured Profile Manager UI.

--- src/webapp/cass.manager/generated-js/cass/manager/AppSettings.js
+++
@@ -1,4 +0,0 @@
-var AppSettings = function() {};
-AppSettings = stjs.extend(AppSettings, null, [], function(constructor, prototype) {
-    constructor.returnLoginMessage = "For Your Security, You are Logged Out on Page Reload. Please Enter Your Credentials to Continue Logged In.";
-}, {}, {});
--- src/webapp/cass.manager/generated-js/cass/manager/other/KeyManager.js
+++
@@ -1,8 +0,0 @@
-var KeyManager = function() {
-    EcView.call(this);
-};
-KeyManager = stjs.extend(KeyManager, EcView, [], function(constructor, prototype) {
-    prototype.display = function(containerId, callback) {
-        console.error("Not Implemented Yet");
-    };
-}, {}, {});
