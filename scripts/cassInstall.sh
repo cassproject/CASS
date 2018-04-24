e0fb2f37be37cb12000294084f3e301bc227f9dc
Ronald Ray <fritz.ray@eduworks.com>
2018-04-24 16:29:23 -0700

    Submodules.

--- scripts/cassInstall.sh
+++ scripts/cassInstall.sh
@@ -238,7 +238,7 @@ fi
 
 echo -----
 echo Downloading CASS Repo...
-git clone --recursive https://github.com/cassproject/CASS -b $branch
+git clone --recurse-submodules https://github.com/cassproject/CASS -b $branch
 cd CASS
 echo Compiling CASS...
 mvn -q clean install
