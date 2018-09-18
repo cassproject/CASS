6596afd930e0af0ca4c5a65c180afbc0aa5aabc6
Tom Buskirk <tom.buskirk@eduworks.com>
2018-09-18 16:15:28 -0500

    Framework Explorer Integration Phase 1
    Framework Explorer Integration Phase 1

--- src/main/webapp/cass-ui/js/page/cass-ui-framework-exp-ctr.js
+++ src/main/webapp/cass-ui/js/page/cass-ui-framework-exp-ctr.js
@@ -5,6 +5,9 @@
 //**************************************************************************************************
 // Constants
 
+const FWK_EXP_IFRAME = "#fwkExpIFrame";
+const  FWK_EXP_IFRAME_SOURCE = "../cass-viewer/cass-ui-framework-exp.html?user=wait&origin=";
+
 //**************************************************************************************************
 // Variables
 
@@ -20,6 +23,15 @@ function sendIdentityInitializeMessage() {
         pemParm: loggedInPpkPem
     }), window.location.origin);
 }
+
+$(FWK_EXP_IFRAME).ready(function() {
+    $(window).on("message", function(event) {
+        if (event.originalEvent.data.message == "waiting") {
+            sendIdentityInitializeMessage();
+        };
+    });
+});
+$(FWK_EXP_IFRAME).attr("src", FWK_EXP_IFRAME_SOURCE + window.location.origin);
 
 function init() {
     loadCassUiSessionState();
