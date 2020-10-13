e6ec69914cb686c2391110c9cd82f15cc3e5f4c0
Fritz Ray <fritz.ray@eduworks.com>
2020-10-13 07:59:58 -0700

    Removed scripts/node folder.

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# Scripts Directory
+
+This directory contains scripts that are not part of the CaSS installed binary.
+
+/cassInstall.sh is a script to install CaSS on Debian or Fedora based systems.
--- scripts/node/configureDebian.sh
+++
@@ -1,6 +0,0 @@
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
-sudo apt-get install -y nodejs
-npm install node-jquery-xhr
-npm install node-forge
-npm install form-data
-npm install antlr4
