8a32aee5be3d8309a800a010e63dda5e9a0693f4
Ronald Ray <fritz.ray@eduworks.com>
2023-02-08 15:19:51 -0800

    Linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,26 @@
 module.exports = {
-  'env': {
-    'es2021': true,
-    'node': true,
-  },
-  'extends': [
-    'google',
-  ],
-  'overrides': [
-  ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-    'sourceType': 'module',
-  },
-  'plugins': [
-  ],
-  'rules': {
-  },
+    'env': {
+        'es2021': true,
+        'node': true,
+    },
+    'extends': [
+        'google',
+    ],
+    'overrides': [
+    ],
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'script',
+    },
+    'plugins': [
+    ],
+    'rules': {
+        'linebreak-style': 'off',
+        'max-len': 'off',
+        'no-invalid-this': 'off',
+        'indent': ['error', 4],
+        'prefer-const': 'off',
+        'require-jsdoc': 'off',
+        'camelcase': 'off',
+    },
 };
--- src/main/server/skyId.test.js
+++ src/main/server/skyId.test.js
@@ -1,70 +1,53 @@
-let axios = require("axios");
-let FormData = require("form-data");
-let chai = require("chai");
-const EcCrypto = require("cassproject/src/com/eduworks/ec/crypto/EcCrypto");
-const EcAes = require("cassproject/src/com/eduworks/ec/crypto/EcAes");
-const EcRemoteLinkedData = require("cassproject/src/org/cassproject/schema/general/EcRemoteLinkedData");
-const EcRemoteIdentityManager = require("cassproject/src/org/cassproject/ebac/identity/remote/EcRemoteIdentityManager");
-const EcIdentityManager = require("cassproject/src/org/cassproject/ebac/identity/EcIdentityManager");
-const EcIdentity = require("cassproject/src/org/cassproject/ebac/identity/EcIdentity");
-const EcPpk = require("cassproject/src/com/eduworks/ec/crypto/EcPpk");
+global.axios = require('axios');
+let chai = require('chai');
+const EcAes = require('cassproject/src/com/eduworks/ec/crypto/EcAes');
+const EcRemoteIdentityManager = require('cassproject/src/org/cassproject/ebac/identity/remote/EcRemoteIdentityManager');
+const EcIdentityManager = require('cassproject/src/org/cassproject/ebac/identity/EcIdentityManager');
+const EcIdentity = require('cassproject/src/org/cassproject/ebac/identity/EcIdentity');
+const EcPpk = require('cassproject/src/com/eduworks/ec/crypto/EcPpk');
 
-let hrtime = function() {
-    try {
-        return [Math.round(performance.now()/1000), performance.now() * 1000];
-    } catch (e) {
-        // Eat quietly.
-    }
-    return process.hrtime();
-};
+let assert = chai.assert;
 
-var should = chai.should();
-var expect = chai.expect;
-var assert = chai.assert;
-
-
-describe("SkyID Adapter", function() {
+describe('SkyID Adapter', function() {
     this.timeout(30000);
     let username = EcAes.newIv(6);
     let password = EcAes.newSecret(6);
     let newPassword = EcAes.newSecret(6);
-    let name = "Test User";
-    let email = username + "@test.cassproject.org";
+    let name = 'Test User';
     let ident = null;
 
     it('create user', async () => {
         let rld = new EcRemoteIdentityManager();
-        rld.server = "http://localhost/api/";
-        await rld.configureFromServer(null,null);
-        rld.startLogin(username,password);
+        rld.server = 'http://localhost/api/';
+        await rld.configureFromServer(null, null);
+        rld.startLogin(username, password);
         let im = new EcIdentityManager();
-        await rld.create(null,null,im);
+        await rld.create(null, null, im);
         im = await rld.fetch();
         ident = new EcIdentity();
-        ident.ppk = await EcPpk.generateKeyAsync(null,null);
+        ident.ppk = await EcPpk.generateKeyAsync(null, null);
         ident.displayName = name;
         im.addIdentity(ident);
-        await rld.commit(null,null,im);
+        await rld.commit(null, null, im);
     }).timeout(2000);
 
     it('change password', async () => {
         let rld = new EcRemoteIdentityManager();
-        rld.server = "http://localhost/api/";
-        await rld.configureFromServer(null,null);
-        rld.startLogin(username,password);
+        rld.server = 'http://localhost/api/';
+        await rld.configureFromServer(null, null);
+        rld.startLogin(username, password);
         let im = await rld.fetch();
-        assert.equal(im.ids[0].ppk.toPem(),ident.ppk.toPem());
-        rld.changePassword(username,password,newPassword);
-        await rld.commit(null,null,im);
-    }).timeout(2000);
-    
-    it('load user', async () => {
-        let rld = new EcRemoteIdentityManager();
-        rld.server = "http://localhost/api/";
-        await rld.configureFromServer(null,null);
-        rld.startLogin(username,newPassword);
-        let im = await rld.fetch();
-        assert.equal(im.ids[0].ppk.toPem(),ident.ppk.toPem());
+        assert.equal(im.ids[0].ppk.toPem(), ident.ppk.toPem());
+        rld.changePassword(username, password, newPassword);
+        await rld.commit(null, null, im);
     }).timeout(2000);
 
+    it('load user', async () => {
+        let rld = new EcRemoteIdentityManager();
+        rld.server = 'http://localhost/api/';
+        await rld.configureFromServer(null, null);
+        rld.startLogin(username, newPassword);
+        let im = await rld.fetch();
+        assert.equal(im.ids[0].ppk.toPem(), ident.ppk.toPem());
+    }).timeout(2000);
 });
