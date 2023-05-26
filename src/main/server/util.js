12a09017a401c0b7ba17cfd6053695d28deeca00
kari.glover <kari.glover@eduworks.com>
2023-05-26 08:24:03 -0700

    Update tag

--- src/main/server/util.js
+++ src/main/server/util.js
@@ -26,9 +26,8 @@ global.skyrepoMigrate = async function(after) {
     let elasticState = await httpGet(elasticEndpoint + '/', true, global.elasticHeaders());
     const result = await httpPut({'index.mapping.total_fields.limit': 10000}, elasticEndpoint + '/schema.cassproject.org.0.4.configuration/_settings', 'application/json', global.elasticHeaders());
     if (skyrepoDebug) {
-        global.auditLogger.report(global.auditLogger.LogCategory.NETWORK, global.auditLogger.Severity.DEBUG, 'SkyrepoPutInternalPerm', JSON.stringify(result));
+        global.auditLogger.report(global.auditLogger.LogCategory.NETWORK, global.auditLogger.Severity.DEBUG, 'SkyrepoSetTotalFieldLimits', JSON.stringify(result));
     }
-
     if (elasticState == null) {
         global.auditLogger.report(global.auditLogger.LogCategory.SYSTEM, global.auditLogger.Severity.INFO, 'SkyrepMigrate', 'Waiting for Elasticsearch to appear at '+elasticEndpoint+'...');
         setTimeout(function() {
