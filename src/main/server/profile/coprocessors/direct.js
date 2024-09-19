7b700ad9e63db7a147bcd7a801ba3f193474e487
Fritz Ray <fritz.ray@eduworks.com>
2024-09-19 14:50:48 -0700

    Added direct positive assertion to processor. Updated package.json

---
+++ src/main/server/profile/coprocessors/direct.js
@@ -0,0 +1,51 @@
+//Each of these is being called such that using 'this' refers to the profileProcessor.
+let fetchAssertions = async function() {
+}
+let insertResources = async function() {
+}
+let processAssertions = async function() {
+}
+let postProcessStart = async function(vertices,topLevelVertices, inEdges) {
+}  
+let postProcessEachVertex = function(vertex,vertices,topLevelVertices,inEdges) {
+    console.log(vertex,vertices,topLevelVertices,inEdges);
+    vertices[vertex.id].state.hasDirectPositiveAssertion = false;
+    if (vertices[vertex.id].assertions != null)
+        vertices[vertex.id].state.hasDirectPositiveAssertion = vertices[vertex.id].assertions.filter(a=>a.competency == vertex.getName()).length > 0;
+}   
+let postProcessEachEdge = function(edge,vertices,topLevelVertices,inEdges) {
+}
+let postProcessEachEdgeRepeating = function(edge,vertices,topLevelVertices,inEdges) {
+}
+let postProcessEachVertexRepeating = function(vertex,vertices,topLevelVertices,inEdges) {
+}
+let postProcessProfileBefore = function(profile,vertices,topLevelVertices,inEdges) {
+}
+let postProcessProfileEachElement = function(obj,inEdges,vertices,visited) {
+    // Recur for each child
+    for (const child of obj.children) {
+        const childResults = postProcessProfileEachElement(
+            child, inEdges, vertices, visited
+        );
+    }
+    return {};
+}    
+let postProcessProfileAfter = function(o, profile) {
+}
+
+module.exports = {
+    enabled: true,
+    version: "1.0.0",
+    order: 200,
+    fetchAssertions,
+    insertResources,
+    processAssertions,
+    postProcessStart,
+    postProcessEachVertex,
+    postProcessEachEdge,
+    postProcessEachEdgeRepeating,
+    postProcessEachVertexRepeating,
+    postProcessProfileBefore,
+    postProcessProfileEachElement,
+    postProcessProfileAfter
+}
