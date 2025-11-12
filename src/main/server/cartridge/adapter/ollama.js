8ac8ecb9344a0c4739a849473046201d459311b7
Fritz Ray <fritz.ray@eduworks.com>
2025-11-11 18:37:44 -0800

    Testing framework generation.

--- src/main/server/cartridge/adapter/ollama.js
+++ src/main/server/cartridge/adapter/ollama.js
@@ -21,7 +21,7 @@ async function isOllamaRunning() {
 }
 
 const ollamaApiUrl = 'http://localhost:11434/api/generate';
-let request = async (prompt) => {
+let request = async (prompt,context) => {
     const response = await fetch(ollamaApiUrl, {
         method: 'POST',
         headers: {
@@ -30,40 +30,87 @@ let request = async (prompt) => {
         body: JSON.stringify({
             model: model,
             prompt: prompt,
-            temperature: 0,
-            context: this.context,
+            options:{
+                num_ctx: -1,
+                repeat_last_n: -1,
+                seed: 0,                
+                temperature: 0
+            },
+            context: context || this.context,
             stream: false
         }),
     });
+    console.log("Ollama Prompt: " + prompt);
     const result = await response.json();
     this.context = result.context;
-    return result.response;
+    console.log("Ollama Response: " + result.response.substring(0,100) + "...");
+    return {context:result.context,response:result.response};
 };
 
 // Function to create a competency framework using Ollama
 async function createFramework(name){
     let framework = new EcFramework();
-    await request.call(this,`You are creating a human capabilities framework for "${name}". Please use doctrinal sources where available.`)
-    framework.name = name;
-    framework.description = await request.call(this,"Create a one paragraph description of the framework.");
-    let competencies = await generateThings.call(this,framework, 5,"Task");
+    if (name == null)
+        await request.call(this,`You are creating a human capabilities framework. Please use doctrinal sources where available. Use English. Avoid conjunctions. Be succinct. Answer the question without pontification.`)
+    else
+        await request.call(this,`You are creating a human capabilities framework for "${name}". Please use doctrinal sources where available. Use English. Avoid conjunctions. Be succinct. Answer the question without pontification.`)
+    framework.name = name || "Human Capabilities Framework";
+    let {context,response} = await request.call(this,"Create a one paragraph description of the framework.");
+    framework.description = response;
+    framework.context = context;
+    let competencies = await generateThings.call(this,[], 5,"Task",context, [2,2]);
     return {framework,competencies};
 }
 
-async function generateThings(framework, count, type) {
-    framework.competency = [];
+async function generateThings(parents, count, type, context, levels) {
     let results = [];
-    for (let i = 0; i < count; i++) {
+    let asPartOf = "";
+    if (parents?.length > 0)
+        for (let i = 0; i < parents.length; i++) {
+            let parent = parents[i];
+            if (i == 0)
+                asPartOf = `"${parent.name}".`;
+            else
+                asPartOf += ` as part of "${parent.name}".`;
+        }
+    for (let j = 0; j < count; j++) {
         let competency = new EcCompetency();
         competency.generateId(repo.selectedServerProxy || repo.selectedServer);
-        await request.call(this,`What is a distinct ${type} for this framework?`);
-        competency.name = await request.call(this,"What is the descriptive name of it? Please respond with only the name.");
-        competency.description = await request.call(this,"How would you describe it? Please respond with only one paragraph of text.");
-        framework.competency.push(competency.shortId());
+        if (j == 0)
+        {
+            if (parents == null)
+                competency.name = (await request.call(this,`What is the name of the most important ${type}? Respond with only the name.`,context)).response;
+            else
+                competency.name = (await request.call(this,`What is the name of the most important ${type} required for ${asPartOf}? Respond with only the name.`,context)).response;
+        }
+        else
+        {
+            if (parents == null)
+                competency.name = (await request.call(this,`What is the name of the next most important ${type}? Respond with only the name.`,context)).response;
+            else
+                competency.name = (await request.call(this,`What is the name of the next most important ${type} required for ${asPartOf}? Respond with only the name.`,context)).response;
+        }
+        // competency.name = (await request.call(this,"What is the declarative name? Please respond with only the name.")).response;
+        let {outContext,response} = await request.call(this,"What is the description? Please respond with only one paragraph of text.");
+        competency.description = response;
+        competency.context = outContext;
+        competency.parent = parents?.map(p => p.shortId());
+        competency["dcterms:type"] = type;
+        if (levels.length > 0) {
+            let subThings = await Promise.all([
+                generateThings([...parents,competency], levels[0], "Task",outContext,levels.slice(1)),
+                generateThings([...parents,competency], levels[0], "Knowledge",outContext,levels.slice(1)),
+                generateThings([...parents,competency], levels[0], "Skill",outContext,levels.slice(1)),
+                generateThings([...parents,competency], levels[0], "Natural Ability",outContext,levels.slice(1))
+            ]);
+            //results.push(...subThings.flat());
+            competency.children = subThings.flat();
+        }
         results.push(competency);
     }
     return results;
 }
+
 // Web service endpoint handler
 async function ollamaEndpoint() {
     
